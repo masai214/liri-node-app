f94faabffcb8b7c48acfc09600bf54810a521016
Morpheus <masai.nazaire@gmail.com>
2019-08-27 15:28:59 -0400

    all functions working

--- liri.js
+++ liri.js
@@ -1,3 +1,110 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
+moment().format();
+var fs = require("fs");
+
+
+var searchTerm = process.argv[2];
+var input = process.argv.slice(3).join(" ");
+
+switch (searchTerm) {
+    case "concert-this":
+        concertThis(input);
+        break;
+    case "spotify-this-song":
+        spotifySong(input);
+        break;
+    case "movie-this":
+        movieThis(input);
+        break;
+    case "do-what-it-says":
+        whatItSays(input);
+        break;
+};
+
+function concertThis(input) {
+    console.log(input);
+    if (!input) {
+        input = "dababy";
+    }
+    axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            for (var i = 0; i < response.data.length; i++) {
+
+                
+
+                var concertResults =
+                    "\nVenue Name: " + response.data[i].venue.name +
+                    "\nVenue Location: " + response.data[i].venue.city +
+                    "\nDate of the Event: " + moment(response.data[i].datetime).format("MM/DD/YYYY");
+                console.log(concertResults);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+
+}
+
+
+
+function spotifySong(input) {
+    if (!input) {
+        input = "Through the Wire";
+    }
+    spotify
+        .search({ type: 'track', query: input })
+        .then(function (response) {
+            for (var i = 0; i < 5; i++) {
+                var spotifyResults =
+                    "\nArtist(s): " + response.tracks.items[i].artists[0].name +
+                    "\nSong Name: " + response.tracks.items[i].name +
+                    "\nAlbum Name: " + response.tracks.items[i].album.name +
+                    "\nPreview Link: " + response.tracks.items[i].preview_url;
+
+                console.log(spotifyResults);
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+
+function movieThis(input) {
+    if (!input) {
+        input = "pulp fiction";
+    }
+    axios.get("https://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy")
+        .then(function (response) {
+            var movieResults =
+                "\nMovie Title: " + response.data.Title +
+                "\nYear of Release: " + response.data.Year +
+                "\nIMDB Rating: " + response.data.imdbRating +
+                "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                "\nCountry Produced: " + response.data.Country +
+                "\nLanguage: " + response.data.Language +
+                "\nPlot: " + response.data.Plot +
+                "\nActors/Actresses: " + response.data.Actors;
+            console.log(movieResults);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function whatItSays(input) {
+
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        
+        var dataArr = data.split(',');
+         spotifySong(dataArr[1]);
+    })
+}
